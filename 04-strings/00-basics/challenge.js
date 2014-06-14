9c8ec1d48c4e081d3e637a0a17b2a2ddb5baa675
rem{e}koh <git@remekoh.me>
2014-06-14 12:32:28 -0500

    Strings Exercises

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,22 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+  return "Hello " + firstName +",\n\n" + message + "\n\nSincerely,\n + senderName"
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+  return .bigString.substring(startA endA) + bigString.substring(startB, endB); 
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+  return text.indexOf(searchString);
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+  return text.lastIndexOf(searchString);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-
+  +  var searchLength = searchString.length;
+  +  var firstMatch = text.indexOf(searchString) + searchLength;
+  +  var lastMatch = text.lastIndexOf(searchString);
+  +  return text.substring(firstMatch, lastMatch);
 };
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-This is the JavaScript exercise repo to accompany Bloc's Front-end Web Development apprenticeship. 
+This is the JavaScript exercise repo to accompany Bloc's Front-end Web Development apprenticeship.
 
 Follow the environment setup from the Roadmap before solving these exercises.
+
+> Before you start a new exercise, be sure to run `git pull bloc master` from your virtual machine's command line. This will pull the master branch of this repo into your local repo and merge updates with your changes. Updates to this repo will be pushed occasionally (when we add or fix new exercises, tests, etc.), and `git pull bloc master` will ensure that you are working with the latest exercises. You may also "watch" this repo to get email notifications of updates and issues.
