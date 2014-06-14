bd2ecbb6cef953daf6b53be64688390bef8fa376
rem{e}koh <git@remekoh.me>
2014-06-14 13:27:46 -0500

    Strings Checkpoint

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,9 +1,9 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-  return "Hello " + firstName +",\n\n" + message + "\n\nSincerely,\n + senderName"
+  return "Hello " + firstName +",\n\n" + message + "\n\nSincerely,\n" + senderName;
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-  return .bigString.substring(startA endA) + bigString.substring(startB, endB); 
+  return bigString.substring(startA, endA) + bigString.substring(startB, endB); 
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
@@ -15,8 +15,8 @@ module.exports.findLastMatch = function(text, searchString) {
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-  +  var searchLength = searchString.length;
-  +  var firstMatch = text.indexOf(searchString) + searchLength;
-  +  var lastMatch = text.lastIndexOf(searchString);
-  +  return text.substring(firstMatch, lastMatch);
+   var searchLength = searchString.length;
+   var firstMatch = text.indexOf(searchString) + searchLength;
+   var lastMatch = text.lastIndexOf(searchString);
+   return text.substring(firstMatch, lastMatch);
 };
