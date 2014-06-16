1b0f393e2be20a62c0960ec2c141992754d32005
rem{e}koh <git@remekoh.me>
2014-06-16 08:15:58 -0500

    Objects Checkpoint

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,29 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = {
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  }
+  return course;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  var exObj = object;
+  exObj[newProp] = newValue;
+  return object;
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(r) {
+  var letter = r;
+  return 'Hello ' + letter.recipient + ',\n\n' + letter.msg + '\n\nSincerely,\n' + letter.sender;
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+  var products = {
+    'MacBook Air': 999,
+    'MacBook Pro': 1299,
+    'Mac Pro': 2499,
+    'Apple Sticker': 1
+  };
+  return products[item] <= money;
+};
