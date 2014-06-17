160bfb99f2a75355f6e39335d333430fbb090dd9
rem{e}koh <git@remekoh.me>
2014-06-17 10:13:15 -0500

    Loops Checkpoint

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,20 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array) {
+  var num = 0;
+  for (var count = 0; count < array.length; count++) {
+  num = num + array[count];
+  }
+  return num;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+  var string = inputString.toLowerCase().split(",");
+  return string;
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(inputData) {
+  var array = [];
+  for (var i = 0; i < inputData.length; i++) {
+  array.push(i + " is " + inputData[i]);
+  }
+  return array;
+};
